5438906899afd1bcc73c0557d0c95a40c1c3977c
Patricio de la Guardia <padlgt01@gmail.com>
2018-06-19 09:55:21 -0400

    Add some styling

---
+++ mockDB.js
@@ -0,0 +1,86 @@
+module.exports = [
+  {
+    "first_name": "Timothy",
+    "last_name": "Robinson",
+    "job": "Illustrator",
+    "address": "450 Ryan Mountain Apt. 359\nDanielhaven, GU 39375-2283",
+    "image_url": "https://placeholdit.imgix.net/~text?txtsize=55&txt=681x635&w=681&h=635"
+  },
+  {
+    "first_name": "Michael",
+    "last_name": "Mendoza",
+    "job": "Catering manager",
+    "address": "PSC 1692, Box 6104\nAPO AE 15778",
+    "image_url": "https://dummyimage.com/806x708"
+  },
+  {
+    "first_name": "Alison",
+    "last_name": "Clements",
+    "job": "Advertising art director",
+    "address": "498 Edwards Forges\nNorth Patrickchester, OR 07516",
+    "image_url": "https://www.lorempixel.com/268/83"
+  },
+  {
+    "first_name": "Douglas",
+    "last_name": "Wells",
+    "job": "Water quality scientist",
+    "address": "Unit 8298 Box 3485\nDPO AE 45215-9914",
+    "image_url": "https://www.lorempixel.com/212/719"
+  },
+  {
+    "first_name": "Raymond",
+    "last_name": "Lee",
+    "job": "Radiographer, diagnostic",
+    "address": "1986 Joshua Road Suite 903\nEast Maureenland, AZ 04022-3391",
+    "image_url": "https://placeholdit.imgix.net/~text?txtsize=55&txt=394x823&w=394&h=823"
+  },
+  {
+    "first_name": "Manuel",
+    "last_name": "Anderson",
+    "job": "Librarian, public",
+    "address": "7119 Harris River Apt. 843\nSouth Willie, AZ 53852",
+    "image_url": "https://placeholdit.imgix.net/~text?txtsize=55&txt=669x384&w=669&h=384"
+  },
+  {
+    "first_name": "Carol",
+    "last_name": "Gibbs",
+    "job": "Horticultural therapist",
+    "address": "1031 Moore Ports Suite 201\nNew Kathy, MS 77776",
+    "image_url": "https://dummyimage.com/144x322"
+  },
+  {
+    "first_name": "Jennifer",
+    "last_name": "Roach",
+    "job": "Scientist, audiological",
+    "address": "006 Dylan Glens\nStephaniemouth, AZ 18667",
+    "image_url": "https://www.lorempixel.com/368/241"
+  },
+  {
+    "first_name": "Thomas",
+    "last_name": "Wright",
+    "job": "Agricultural consultant",
+    "address": "64013 Tiffany Mews\nSouth Ashleyview, MP 87796",
+    "image_url": "https://www.lorempixel.com/547/855"
+  },
+  {
+    "first_name": "Courtney",
+    "last_name": "Johnson",
+    "job": "Chief Operating Officer",
+    "address": "17267 Theresa Lodge\nDavidberg, MN 79077",
+    "image_url": "https://placeholdit.imgix.net/~text?txtsize=55&txt=5x1023&w=5&h=1023"
+  },
+  {
+    "first_name": "Joseph",
+    "last_name": "Silva",
+    "job": "Holiday representative",
+    "address": "568 Mahoney Pass Suite 254\nNew Samuelfort, MS 53496",
+    "image_url": "https://placeholdit.imgix.net/~text?txtsize=55&txt=289x492&w=289&h=492"
+  },
+  {
+    "first_name": "Justin",
+    "last_name": "Austin",
+    "job": "Interior and spatial designer",
+    "address": "Unit 2286 Box 7304\nDPO AP 69248-5085",
+    "image_url": "https://dummyimage.com/624x698"
+  }
+]
