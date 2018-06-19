80282b8a5b44962d0c1eb3af68fb4fa6bea249ee
Patricio de la Guardia <padlgt01@gmail.com>
2018-06-19 16:42:22 -0400

    Server serves static content instead of requesting data from Don's server

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
 const express = require("express");
 const axios = require("axios");
-const data = require('./mockDB');
+let data = require("./mockDB");
+
+data = data.map(person => {
+  person = Object.assign({}, person, {
+    firstName: person.first_name,
+    lastName: person.last_name,
+    imageUrl: person.image_url
+  });
+	delete person.first_name;
+	delete person.last_name;
+	delete person.image_url;
+  return person;
+});
 
 const app = express();
 
 app.use(express.static(`${__dirname}/public`));
 
-app.get('/', (req, res) => res.end());
+app.get("/", (req, res) => res.end());
 
 app.get("/users", async (req, res) => {
   try {
     res.send(data);
-  }catch(err) {
+  } catch (err) {
     res.status(500);
     res.end();
   }
